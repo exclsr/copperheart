030b5a593502334e12f995ae78f2d49808abdace
Phil <github@exclsr.com>
2013-03-23 20:30:12 -0700

    Stripe: Add simple input for creating token
    Create a single-use token via Stripe.js, based
    on a credit card, that is ready to be passed
    to Stripe's servers for processing.

--- app/public/js/controllers.js
+++ app/public/js/controllers.js
@@ -3,7 +3,33 @@
 /* Controllers */
 
 
-function HelloCtrl() {}
-HelloCtrl.$inject = [];
+function HelloCtrl($scope) {
+
+	$scope.submitPayment = function() {
+		
+		var creditCard = {
+			number: $scope.cc.number,
+			cvc: $scope.cc.cvc,
+			exp_month: $scope.cc.expMonth,
+			exp_year: $scope.cc.expYear
+		};
+
+		var stripeResponseHandler = function(status, response) {
+			if (response.error) {
+				// Show the errors on the form
+				$scope.errorMessage = response.error.message;
+			} else {
+				var stripeToken = response.id;
+				$scope.stripeToken = stripeToken;
+				// Force the UI to update, like a sad person.
+				$scope.$apply(); 
+			}
+		};
+
+		// TODO: Learn how to inject something like Stripe, then do so.
+		Stripe.createToken(creditCard, stripeResponseHandler);
+	};
+}
+HelloCtrl.$inject = ['$scope'];
 
 

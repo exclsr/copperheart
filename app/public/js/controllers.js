9b268a4ea7c9aafbb5e33a370a7fe2820bd43846
Phil <github@exclsr.com>
2013-03-28 16:16:45 -0700

    Sum the one-time price and recurring price
    There are two types of contributions: one-time
    and recurring each month. Calculate and show
    what each of these are, as things are selected.

--- app/public/js/controllers.js
+++ app/public/js/controllers.js
@@ -35,7 +35,25 @@ function HelloCtrl($scope, $http) {
 		},
 	];
 
-	$scope.totalPrice = function() {
+	var perMonthMultiplier = function (frequency) {
+
+		switch (frequency) {
+			case 'day': 
+				return 365.0 / 12.0;
+
+			case 'week': 
+				// There are 4 and 1/3 weeks 
+				// each month, on average.
+				return 52.0 / 12.0;
+
+			case 'month':
+			default:
+				return 1.0;
+		}
+
+	};
+
+	$scope.priceNow = function() {
 		var totalPrice = 0;
 
 		angular.forEach($scope.things, function (thing) {
@@ -44,7 +62,32 @@ function HelloCtrl($scope, $http) {
 			}
 		});
 
-		return totalPrice;
+		// TODO: Not sure if formatting should be in here.
+		if (totalPrice === 0) {
+			return totalPrice;
+		}
+
+		return totalPrice.toFixed(2);
+	};
+
+	$scope.pricePerMonth = function() {
+		var pricePerMonth = 0;
+
+		angular.forEach($scope.things, function (thing) {
+			var itemPrice = 0;
+
+			if (thing.canHaz && thing.recurring) {
+				itemPrice = thing.price * perMonthMultiplier(thing.frequency);
+				pricePerMonth += itemPrice;
+			}
+		});
+
+		// TODO: Not sure if formatting should be in here.
+		if (pricePerMonth === 0) {
+			return pricePerMonth;
+		}
+
+		return pricePerMonth.toFixed(2);
 	};
 }
 HelloCtrl.$inject = ['$scope', '$http'];

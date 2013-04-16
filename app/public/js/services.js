a57b275e4b85c159a72ea05ec355975fe5990793
Phil <github@exclsr.com>
2013-04-16 12:32:21 -0700

    Version 0.3
    The logged in patron can now edit his
    or her profile data via the website,
    and it's stored in CouchDB.
    
    Most of the hard-coded profile stuff
    is now gone.
    
    And, the typography is satisfying
    to my eye.

--- app/public/js/services.js
+++ app/public/js/services.js
@@ -6,7 +6,7 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.2').
+  value('version', '0.3').
   factory('activeContribution', function() {
   	return {
   		things: []

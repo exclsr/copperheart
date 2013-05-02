e50c9be2a545032b1d15925b2f913ebb94d86bf6
Phil <github@exclsr.com>
2013-05-01 19:00:29 -0700

    Add entrance

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -4,12 +4,13 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngSanitize', 'ui']).
   config(['$routeProvider', function($routeProvider) {
-  	// Not sure which order is best, so let's do both!
+  	$routeProvider.when('/entrance', {templateUrl: 'partials/entrance.html', controller: EntranceCtrl});
+    // Not sure which order is best, so let's do both!
     $routeProvider.when('/:who/hello', {templateUrl: 'partials/hello.html', controller: HelloCtrl});
     $routeProvider.when('/hello/:who', {templateUrl: 'partials/hello.html', controller: HelloCtrl});
     $routeProvider.when('/hello', {templateUrl: 'partials/hello.html', controller: HelloCtrl});
     $routeProvider.when('/contribute', {templateUrl: 'partials/contribute.html', controller: ContributeCtrl});
     $routeProvider.when('/edit', {templateUrl: 'partials/edit.html', controller: EditCtrl});
     $routeProvider.when('/edit/contributions', {templateUrl: 'partials/editContributions.html', controller: EditContributionsCtrl});
-    $routeProvider.otherwise({redirectTo: '/hello/phil'});
+    $routeProvider.otherwise({redirectTo: '/entrance'});
   }]);
---
+++ app/public/js/controllers/entrance.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function EntranceCtrl(session, $scope, $http) {
+
+
+}
+EntranceCtrl.$inject = ['session', '$scope', '$http'];

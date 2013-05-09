ffde1854edddc542ed606e86f781944a459d51e1
Phil <github@exclsr.com>
2013-05-09 01:41:12 -0700

    Version 0.5
    There's now an entrance / home page, and
    a contribution flow that mostly makes sense.
    There is a thank-you page, albeit empty.
    
    Things can be edited and rearranged. Patrons
    can see who they're contributing to. There's
    an about page that starts to explain what's up.
    
    Heart favicon.

--- app/public/js/services.js
+++ app/public/js/services.js
@@ -3,7 +3,7 @@
 /* Services */
 
 angular.module('myApp.services', []).
-	value('version', '0.4').
+	value('version', '0.5').
 	factory('session', function() {
 		// session: use localStorage to maintain session
 		// state across visits to the page and refreshes.

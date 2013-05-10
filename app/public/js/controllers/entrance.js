6aca96ed109fde88800a27745590324f900d328d
Phil <github@exclsr.com>
2013-05-10 14:03:07 -0700

    Performance: Entrance: Get image in parallel
    Get the profile image in parallel to the profile
    data, to basically halve the load time of the page.

--- app/public/js/controllers/entrance.js
+++ app/public/js/controllers/entrance.js
@@ -7,17 +7,26 @@ function EntranceCtrl(session, $scope, $http) {
 
 	$http.get('/entrance/usernames')
 	.success(function (usernames) {
-
 		angular.forEach(usernames, function (username) {
+			// Performance: In order to parallelize getting
+			// the profile image and the profile data, just
+			// go for it and set the imageUrl based on the
+			// username.
+			//
+			// The trade off doing it this way is that we
+			// assume all the usernames are valid, for the
+			// benefit of twice-as-fast load times.
+			var profile = {};
+			profile.username = username;
+			profile.imageUrl = "/profile/" + username + "/image";
+			profiles.push(profile);
+			$scope.profiles = profiles;
 
 			$http.get('/profile/' + username)
 			.success(function (p) {
 				if (p.username) {
-					var profile = p;
-					profile.imageUrl = "/profile/" + username + "/image";
-					profiles.push(profile);
-
-					$scope.profiles = profiles;
+					profile.name = p.name;
+					profile.communities = p.communities;
 				}
 			});
 		});

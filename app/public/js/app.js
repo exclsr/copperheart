a251a17f853e48ba6d24159760cd62bae18e7825
Phil <github@exclsr.com>
2013-08-03 22:57:33 -0700

    Fix: Use absolute urls instead of relative urls
    If we want to use Angular's html5Mode for
    routes, we'll need to stop using relative urls
    like the ones fixed up in this commit.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -6,11 +6,11 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngSanitize', 'ui']).
   config(['$routeProvider', function($routeProvider) {
     
-    var hello = { templateUrl: 'partials/hello.html', controller: HelloCtrl };
-    var edit = { templateUrl: 'partials/edit.html', controller: EditCtrl };
+    var hello = { templateUrl: '/partials/hello.html', controller: HelloCtrl };
+    var edit = { templateUrl: '/partials/edit.html', controller: EditCtrl };
 
-  	$routeProvider.when('/entrance', {templateUrl: 'partials/entrance', controller: EntranceCtrl});
-    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: AboutCtrl});
+  	$routeProvider.when('/entrance', {templateUrl: '/partials/entrance', controller: EntranceCtrl});
+    $routeProvider.when('/about', {templateUrl: '/partials/about.html', controller: AboutCtrl});
     // Not sure which order is best, so let's do both!
     $routeProvider.when('/:who/hello', hello);
     $routeProvider.when('/hello/:who', hello);
@@ -19,10 +19,10 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
     $routeProvider.when('/hello/:who/community/:communityId', hello);
     $routeProvider.when('/hello/:who/support', hello);
     $routeProvider.when('/hello', hello);
-    $routeProvider.when('/contribute', {templateUrl: 'partials/contribute.html', controller: ContributeCtrl});
-    $routeProvider.when('/contribute/thanks', {templateUrl: 'partials/thanks.html', controller: ThanksCtrl});
-    $routeProvider.when('/edit', {templateUrl: 'partials/edit.html', controller: EditCtrl});
-    $routeProvider.when('/edit/contributions', {templateUrl: 'partials/editContributions.html', controller: EditContributionsCtrl});
-    $routeProvider.when('/edit/:subEdit', {templateUrl: 'partials/edit.html', controller: EditCtrl});
+    $routeProvider.when('/contribute', {templateUrl: '/partials/contribute.html', controller: ContributeCtrl});
+    $routeProvider.when('/contribute/thanks', {templateUrl: '/partials/thanks.html', controller: ThanksCtrl});
+    $routeProvider.when('/edit', {templateUrl: '/partials/edit.html', controller: EditCtrl});
+    $routeProvider.when('/edit/contributions', {templateUrl: '/partials/editContributions.html', controller: EditContributionsCtrl});
+    $routeProvider.when('/edit/:subEdit', {templateUrl: '/partials/edit.html', controller: EditCtrl});
     $routeProvider.otherwise({redirectTo: '/entrance'});
   }]);
